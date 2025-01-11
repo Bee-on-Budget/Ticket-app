b1894232b331792c1cca82b6c97584d892c56b09
Salwan Arar <salwan.arar@gmail.com>
2025-01-11 11:29:10 +0200

    First Commit

--- lib/src/modules/login_screen.dart
+++ lib/src/modules/login_screen.dart
@@ -8,23 +8,25 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        spacing: 3,
-        children: [
-          SizedBox(height: 50),
-          Text(
-            "Login Screen",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          SizedBox(height: 30),
-          FormFieldOutline(
-            label: "Email",
-            controller: _emailController,
-          ),
-        ],
+    return Container(
+      child: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          spacing: 3,
+          children: [
+            SizedBox(height: 50),
+            Text(
+              "Login Screen",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            SizedBox(height: 30),
+            FormFieldOutline(
+              label: "Email",
+              controller: _emailController,
+            ),
+          ],
+        ),
       ),
     );
   }
