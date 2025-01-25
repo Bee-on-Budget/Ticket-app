87063e3d308b8e63456a2db989e3359158d1e202
Salwan Arar <salwan.arar@gmail.com>
2025-01-25 20:33:38 +0200

    Restructure main code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
-import 'src/modules/screens/home_screen.dart';
-import 'src/modules/screens/login_screen.dart';
-import 'src/modules/screens/register_screen.dart';
+import './src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,24 +11,5 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Firebase Auth & Firestore',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegistrationScreen(),
-        '/home': (context) => HomeScreen(),
-      },
-    );
-  }
+  runApp(const App());
 }
