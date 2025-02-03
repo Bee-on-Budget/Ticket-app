4870b22a2032fc8c2913af31c8b658d446980597
Salwan Arar <salwan.arar@gmail.com>
2025-02-03 22:25:01 +0200

    Clean up the code

--- lib/src/modules/screens/home/home_page.dart
+++ lib/src/modules/screens/home/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ticket_app/src/service/auth_service.dart';
-import 'package:ticket_app/src/widgets/ticket_card.dart';
 
 import '../tickets/new_ticket_screen.dart';
+import '../../../service/auth_service.dart';
+import '../../../widgets/ticket_card.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -83,128 +83,4 @@ class HomePage extends StatelessWidget {
       },
     );
   }
-
-  // Widget _buildTicketItem(BuildContext context, DocumentSnapshot ticket) {
-  //   final data = ticket.data() as Map<String, dynamic>;
-  //   final createdDate = (data['createdDate'] as Timestamp).toDate();
-  //   final status = data['status'] ?? '';
-  //
-  //   return Card(
-  //     margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-  //     child: ListTile(
-  //       title: Text(data['title']),
-  //       subtitle: Text(DateFormat.yMMMd().format(createdDate)),
-  //       trailing: Chip(
-  //         label: Text(
-  //           _getStatusString(status),
-  //           style: TextStyle(color: Colors.white),
-  //         ),
-  //         backgroundColor: _getStatusColor(status),
-  //       ),
-  //       onTap: () {
-  //         Navigator.push(
-  //           context,
-  //           MaterialPageRoute(
-  //             builder: (context) => TicketDetailScreen(ticket: data),
-  //           ),
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
-
-  // String _getStatusString(String status) {
-  //   switch (status) {
-  //     case 'Open':
-  //     case 'In Progress':
-  //     case 'Closed':
-  //       return status;
-  //     default:
-  //       return 'Unknown';
-  //   }
-  // }
-
-  // Color _getStatusColor(String status) {
-  //   switch (status) {
-  //     case 'Open':
-  //       return Colors.green;
-  //     case 'In Progress':
-  //       return Colors.orange;
-  //     case 'Closed':
-  //       return Colors.red;
-  //     default:
-  //       return Colors.grey;
-  //   }
-  // }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:ticket_app/src/modules/screens/home_screen.dart';
-//
-// class HomePage extends StatelessWidget {
-//   HomePage({super.key});
-//
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//
-//   Future<DocumentSnapshot> _getUserData(String uid) {
-//     return FirebaseFirestore.instance.collection('users').doc(uid).get();
-//   }
-//
-//   void _signOut(BuildContext context) async {
-//     final navigator = Navigator.of(context);
-//     final scaffoldMessenger = ScaffoldMessenger.of(context);
-//     try {
-//       await _auth.signOut();
-//       navigator.pushReplacementNamed('/login');
-//     } catch (e) {
-//       scaffoldMessenger.showSnackBar(
-//         SnackBar(content: Text('Error signing out: $e')),
-//       );
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     User? user = _auth.currentUser;
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Home'),
-//         actions: [
-//           IconButton(
-//             icon: const Icon(Icons.logout),
-//             onPressed: () => _signOut(context),
-//           ),
-//         ],
-//       ),
-//       body: user == null
-//           ? const Center(child: Text('No user signed in.'))
-//           : FutureBuilder<DocumentSnapshot>(
-//         future: _getUserData(user.uid),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return const Center(child: CircularProgressIndicator());
-//           }
-//           if (snapshot.hasError) {
-//             return Center(child: Text('Error: ${snapshot.error}'));
-//           }
-//           if (!snapshot.hasData || snapshot.data == null) {
-//             return const Center(child: Text('User data not found.'));
-//           }
-//
-//           // Safe way to get data from FireStore
-//           var userData = snapshot.data!.data() as Map<String, dynamic>?;
-//           if (userData == null) {
-//             return Center(child: Text('No user data available.\n${user.uid}'));
-//           }
-//
-//           // String fullName = userData['fullName'] ?? 'User';
-//           // String role = userData['role'] ?? 'user';
-//
-//           return const HomeScreen();
-//         },
-//       ),
-//     );
-//   }
-// }
