bface24362ab6f42a5132a2dc06dac794e69f945
Salwan Arar <salwan.arar@gmail.com>
2025-02-03 20:10:18 +0200

    adding theme_config.dart

---
+++ lib/src/config/themes/theme_config.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var themeConfig = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Color(0xFF3D4B3F),
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Color(0xFF3D4B3F),
+  ),
+);
