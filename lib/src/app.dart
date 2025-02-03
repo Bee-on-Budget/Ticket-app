2f4c359bae6aaccbf1bc011cb0ef79376ff7943e
Salwan Arar <salwan.arar@gmail.com>
2025-02-03 20:05:05 +0200

    Adding configuration theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/src/config/themes/theme_config.dart';
 import 'modules/screens/auth_page.dart';
 import 'modules/screens/home/home_page.dart';
 import 'modules/screens/login/login_page.dart';
@@ -8,16 +9,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ticket App',
       initialRoute: '/',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0xFF3D4B3F),
-        ),
-      ),
+      theme: themeConfig,
       routes: {
         '/': (context) => const AuthPage(),
         '/login': (context) => const LoginPage(),
