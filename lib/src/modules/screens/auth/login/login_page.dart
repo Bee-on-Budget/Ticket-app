d29bf90e8d2404555ede3d29282c6a9be1f134a0
Salwan Arar <salwan.arar@gmail.com>
2025-02-09 17:13:03 +0200

    Rearranging

--- lib/src/modules/screens/auth/login/login_page.dart
+++ lib/src/modules/screens/auth/login/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
-import './login_screen.dart';
+import 'login_screen.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
