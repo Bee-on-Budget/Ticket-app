6e5987b645d1bd4beb6a92971ff17a2cce582924
Salwan Arar <salwan.arar@gmail.com>
2025-02-11 01:56:38 +0200

    Modify to support WebFile, and add PaymentMethod

--- lib/src/modules/screens/tickets/new_ticket_screen.dart
+++ lib/src/modules/screens/tickets/new_ticket_screen.dart
@@ -1,13 +1,17 @@
 import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import '../../../config/enums/payment_methods.dart';
 import '../../../service/synchronized_time.dart';
 import '../../../widgets/form_field_outline.dart';
 import '../../../widgets/submit_button.dart';
+import '../../models/app_file.dart';
 
 class NewTicketScreen extends StatefulWidget {
   const NewTicketScreen({super.key});
@@ -18,17 +22,17 @@ class NewTicketScreen extends StatefulWidget {
 
 class _NewTicketScreenState extends State<NewTicketScreen> {
   late final GlobalKey<FormState> _formKey;
-  late final TextEditingController _titleController;
-  late final TextEditingController _descriptionController;
+  String _description = "";
+  String _title = "";
+  PaymentMethods? _paymentMethod;
   late final FirebaseFirestore _firestore;
   late final FirebaseAuth _auth;
   late final FirebaseStorage _storage;
-
   static const _maxFiles = 10;
   static const _maxFileSizeMB = 5;
   static const _maxFileSizeBytes = _maxFileSizeMB * 1024 * 1024;
 
-  final List<File> _selectedFiles = [];
+  final List<AppFile> _selectedFiles = [];
   bool _isUploading = false;
 
   @override
@@ -38,38 +42,94 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
   }
 
   @override
-  void dispose() {
-    finish();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New Ticket')),
+      appBar: AppBar(
+        title: const Text('New Ticket'),
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .primaryContainer,
+      ),
+      backgroundColor: Theme
+          .of(context)
+          .colorScheme
+          .surface,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
             key: _formKey,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               spacing: 20,
               children: [
                 FormFieldOutline(
-                  controller: _titleController,
-                  prefixIcon: Icons.title,
-                  hintText: 'Title',
+                  label: "Title",
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Title can't be empty";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _title = value!;
+                    },
+                  ),
                 ),
                 FormFieldOutline(
-                  controller: _descriptionController,
-                  maxLines: 3,
-                  prefixIcon: Icons.description,
-                  hintText: 'Description',
+                  label: "Description",
+                  child: TextFormField(
+                    maxLines: 6,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Field can't be empty";
+                      }
+                      if (value.length < 20) {
+                        return "Description can't be less than 20";
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _description = value!;
+                    },
+                  ),
+                ),
+                const Text(
+                  "Payment Method",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFF8D8D8D),
+                  ),
+                ),
+                DropdownButtonFormField<PaymentMethods>(
+                  items: PaymentMethods.values
+                      .map<DropdownMenuItem<PaymentMethods>>(
+                        (element) =>
+                        DropdownMenuItem(
+                          value: element,
+                          child: Text(element.capitalize),
+                        ),
+                  )
+                      .toList(),
+                  onChanged: (paymentMethod) {
+                    setState(() {
+                      _paymentMethod = paymentMethod;
+                    });
+                  },
+                  validator: (PaymentMethods? paymentMethod) {
+                    if(paymentMethod == null) {
+                      return "You need to select a payment method";
+                    }
+                    return null;
+                  },
                 ),
                 _buildFileUploadSection(),
+                const SizedBox(height: 10),
                 SubmitButton(
                   onPressed: _isUploading ? null : _submitTicket,
-                  buttonText: _isUploading ? 'Uploading...' : 'Submit',
+                  buttonText: _isUploading ? 'Uploading...' : "Create",
                 ),
               ],
             ),
@@ -96,8 +156,15 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
               '${_selectedFiles.length}/$_maxFiles files',
               style: TextStyle(
                 color: _selectedFiles.length >= _maxFiles
-                    ? Theme.of(context).colorScheme.error
-                    : Theme.of(context).textTheme.bodySmall?.color,
+                    ? Theme
+                    .of(context)
+                    .colorScheme
+                    .error
+                    : Theme
+                    .of(context)
+                    .textTheme
+                    .bodySmall
+                    ?.color,
               ),
             ),
           ],
@@ -109,22 +176,23 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
               const Text('Attached Files:'),
               const SizedBox(height: 8),
               ..._selectedFiles.map(
-                (file) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: Row(
-                    children: [
-                      const Icon(Icons.insert_drive_file, size: 20),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: Text(file.path.split('/').last),
+                    (file) =>
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.insert_drive_file, size: 20),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: Text(file.name),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.close, size: 18),
+                            onPressed: () => _removeFile(file),
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: const Icon(Icons.close, size: 18),
-                        onPressed: () => _removeFile(file),
-                      ),
-                    ],
-                  ),
-                ),
+                    ),
               ),
             ],
           ),
@@ -132,11 +200,30 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
     );
   }
 
+  void _showErrorSnackBar(String message) {
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            message,
+            style: TextStyle(
+              color: Theme
+                  .of(context)
+                  .colorScheme
+                  .onPrimaryContainer,
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
   Future<void> _pickFiles() async {
     try {
       final result = await FilePicker.platform.pickFiles(
         allowMultiple: true,
         type: FileType.any,
+        withData: kIsWeb,
       );
 
       if (result != null) {
@@ -144,13 +231,7 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
         final totalFiles = _selectedFiles.length + newFileCount;
 
         if (totalFiles > _maxFiles) {
-          if (mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text("Maximum $_maxFiles files allowed"),
-              ),
-            );
-          }
+          _showErrorSnackBar("Maximum $_maxFiles files allowed");
           return;
         }
 
@@ -159,38 +240,38 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
             .toList();
 
         if (overSizedFiles.isNotEmpty) {
-          if (mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(
-                  "${overSizedFiles.length} file(s) exceed $_maxFileSizeBytes MB limits",
-                ),
-              ),
-            );
-          }
+          _showErrorSnackBar(
+            "${overSizedFiles.length} file(s) exceed "
+                "${_maxFileSizeBytes ~/ 1024 ~/ 1024}MB limits",
+          );
           return;
         }
 
         setState(() {
           _selectedFiles.addAll(
-            result.paths
-                .map(
-                  (path) => File(path!),
-                )
-                .toList(),
+            result.files.map((PlatformFile platformFile) {
+              if (kIsWeb || platformFile.path == null) {
+                return WebFile(
+                  name: platformFile.name,
+                  bytes: platformFile.bytes!,
+                  size: platformFile.size,
+                );
+              }
+              return LocalFile(
+                name: platformFile.name,
+                file: File(platformFile.path!),
+                size: platformFile.size,
+              );
+            }).toList(),
           );
         });
       }
     } catch (e) {
-      if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Error selecting files: ${e.toString()}')),
-        );
-      }
+      _showErrorSnackBar("Error selecting files: ${e.toString()}");
     }
   }
 
-  void _removeFile(File file) {
+  void _removeFile(AppFile file) {
     setState(() {
       _selectedFiles.remove(file);
     });
@@ -199,18 +280,20 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
   Future<void> _submitTicket() async {
     if (!_formKey.currentState!.validate()) return;
 
-    final navigator = Navigator.of(context);
-    final scaffoldMessenger = ScaffoldMessenger.of(context);
     final user = _auth.currentUser;
     if (user == null) return;
 
     setState(() => _isUploading = true);
     try {
+      _formKey.currentState!.save();
       SynchronizedTime.initialize();
       final ticketRef = await _firestore.collection('tickets').add({
         'userId': user.uid,
-        'title': _titleController.text,
-        'description': _descriptionController.text,
+        // 'title': _titleController.text,
+        // 'description': _descriptionController.text,
+        'title': _title,
+        'description': _description,
+        'paymentMethod': _paymentMethod!.capitalize,
         'createdDate': SynchronizedTime.now(),
         'status': 'Open',
       });
@@ -219,14 +302,18 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
         final filesCollection = ticketRef.collection('files');
 
         for (final file in _selectedFiles) {
-          final fileName = file.path.split('/').last;
-          final storageRef =
-              _storage.ref().child('tickets/${ticketRef.id}/files/$fileName');
+          final fileName = file.name;
+          final storageRef = _storage.ref().child(
+            'tickets/${ticketRef.id}/files/$fileName',
+          );
 
-          // Upload file to storage
-          await storageRef.putFile(file);
-          final downloadUrl =
-              await storageRef.getDownloadURL(); // Corrected line
+          if (file is WebFile) {
+            await storageRef.putData(file.bytes);
+          } else if (file is LocalFile) {
+            await storageRef.putFile(file.file);
+          }
+
+          final downloadUrl = await storageRef.getDownloadURL();
 
           SynchronizedTime.initialize();
           await filesCollection.add({
@@ -239,11 +326,11 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
         }
       }
 
-      navigator.pop();
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
     } catch (e) {
-      scaffoldMessenger.showSnackBar(
-        SnackBar(content: Text('Error: ${e.toString()}')),
-      );
+      _showErrorSnackBar('Error: ${e.toString()}');
     } finally {
       setState(() => _isUploading = false);
     }
@@ -251,15 +338,8 @@ class _NewTicketScreenState extends State<NewTicketScreen> {
 
   void init() {
     _formKey = GlobalKey<FormState>();
-    _titleController = TextEditingController();
-    _descriptionController = TextEditingController();
     _firestore = FirebaseFirestore.instance;
     _auth = FirebaseAuth.instance;
     _storage = FirebaseStorage.instance;
-  }
-
-  void finish() {
-    _titleController.dispose();
-    _descriptionController.dispose();
   }
 }
