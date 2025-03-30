7923b97b9f041bd63655cb6aec0cce6e6dd9d6c0
Salwan Arar <salwan.arar@gmail.com>
2025-03-30 21:12:31 +0300

    Normalize the code

--- lib/src/modules/screens/tickets/ticket_detail_screen.dart
+++ lib/src/modules/screens/tickets/ticket_detail_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:ticket_app/src/service/data_service.dart';
 
-import 'file_messaging_page.dart';
+import '../../../service/data_service.dart';
+import '../../../utils/ui/file_list_view.dart';
 import '../../models/ticket.dart';
-import '../../models/ticket_file.dart';
 
 class TicketDetailScreen extends StatelessWidget {
   const TicketDetailScreen({
@@ -19,42 +16,45 @@ class TicketDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Ticket details")),
+      appBar: AppBar(title: const Text("Ticket details")),
       body: StreamBuilder<Ticket>(
-          stream: DataService.getTicketWithFiles(ticketNoFile),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(
-                  'Error loading files: ${snapshot.error}',
-                ),
-              );
-            }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-
-            final ticket = snapshot.data!;
-            return Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 16,
-                horizontal: 30,
+        stream: DataService.getTicketWithFiles(ticketNoFile),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error loading files: ${snapshot.error}',
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 10,
-                children: [
-                  Text(
-                    ticket.title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final ticket = snapshot.data!;
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8,
+              children: [
+                Text(
+                  ticket.title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
                   ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                if (ticket.description.trim().isNotEmpty) ...[
                   Text(
                     ticket.description,
                     style: const TextStyle(
@@ -64,129 +64,61 @@ class TicketDetailScreen extends StatelessWidget {
                     maxLines: 3,
                     overflow: TextOverflow.ellipsis,
                   ),
-                  Row(
-                    children: [
-                      // Date
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Icon(Icons.calendar_today,
-                              size: 16, color: Colors.grey),
-                          const SizedBox(width: 8),
-                          Text(
-                            DateFormat('MMM dd, yyyy')
-                                .format(ticket.createdDate),
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Spacer(), // Adds space between date and status
-                      // Status
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 6,
-                        ),
-                        decoration: BoxDecoration(
-                          color:
-                              ticket.status.getColor().withValues(alpha: 0.1),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text(
-                          ticket.status.toString(),
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: ticket.status.getColor(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Text(
-                    'Attachments:',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Expanded(
-                    child: _buildFileList(ticket),
-                  ),
+                  const SizedBox(height: 4),
                 ],
-              ),
-            );
-          }),
-    );
-  }
-  
-  Widget _buildFileList(Ticket ticket) {
-    final List<TicketFile> files = ticket.files;
-    if (files.isEmpty) {
-      return const Center(child: Text('No attachments'));
-    }
-
-    return ListView.builder(
-      itemCount: files.length,
-      itemBuilder: (context, index) {
-        final file = files[index];
-        final fileName = file.fileName;
-        final isImage = fileName.toLowerCase().endsWith('.jpg') ||
-            fileName.toLowerCase().endsWith('.jpeg') ||
-            fileName.toLowerCase().endsWith('.png');
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 4),
-          child: ListTile(
-            leading: isImage
-                ? CachedNetworkImage(
-                    imageUrl: file.url,
-                    width: 50,
-                    height: 50,
-                    fit: BoxFit.cover,
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error_outline),
-                  )
-                : const SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Icon(
-                      Icons.picture_as_pdf_outlined,
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.calendar_today,
+                          size: 16,
+                          color: Colors.grey,
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          DateFormat('MMM dd, yyyy').format(ticket.createdDate),
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-            title: Text(fileName),
-            subtitle: Text('Attache Id: ${file.refId}'),
-            trailing: file.isThereMsgNotRead
-                ? Icon(
-                    Icons.notification_important_outlined,
-                    color: Theme.of(context).colorScheme.error,
-                  )
-                : const SizedBox(),
-            onTap: () async {
-              await FirebaseFirestore.instance
-                  .collection('tickets')
-                  .doc(ticket.ticketId)
-                  .collection('files')
-                  .doc(file.fileId)
-                  .update({
-                'isThereMsgNotRead': false,
-              });
-              if (context.mounted) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FileMessagingPage(
-                      ticketId: ticket.ticketId,
-                      file: file,
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      decoration: BoxDecoration(
+                        color: ticket.status.getColor().withValues(alpha: 0.1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        ticket.status.toString(),
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: ticket.status.getColor(),
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              }
-            },
-          ),
-        );
-      },
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Attachments:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  child: FileListView(ticket: ticket),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
