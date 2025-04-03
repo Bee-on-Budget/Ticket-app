848336c8c82db194701a556c5ee43165fc85460b
Salwan Arar <salwan.arar@gmail.com>
2025-04-03 16:58:03 +0300

    Adding picking file/s

--- lib/src/service/file_repository.dart
+++ lib/src/service/file_repository.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:web/web.dart' as web;
 
+import '../modules/models/app_file.dart';
 import '../modules/models/ticket_file.dart';
+import 'functions.dart';
+
+const _allowedExtensions = ['png', 'jpg', 'jpeg', 'pdf'];
+const _dialogTitle = 'Select File';
 
 Future<void> handleDownload(BuildContext context, TicketFile file) async {
   try {
@@ -20,7 +28,6 @@ Future<void> handleDownload(BuildContext context, TicketFile file) async {
         ..href = downloadUrl
         ..download = file.fileName
         ..style.display = 'none';
-      html.document.body?.children.add(anchor);
 
       // Add to DOM and trigger click
       web.document.body?.append(anchor);
@@ -44,6 +51,124 @@ Future<void> handleDownload(BuildContext context, TicketFile file) async {
       );
     }
   }
+}
+
+Future<AppFile?> pickAFile(
+  BuildContext context,
+) async {
+  AppFile? selectedFile;
+  try {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      withData: kIsWeb,
+      dialogTitle: _dialogTitle,
+      lockParentWindow: true,
+      allowCompression: false,
+      allowedExtensions: _allowedExtensions,
+    );
+
+    if (result != null) {
+      final platformFile = result.files.first;
+        if (kIsWeb || platformFile.path == null) {
+          selectedFile = WebFile(
+            name: platformFile.name,
+            bytes: platformFile.bytes!,
+            size: platformFile.size,
+          );
+        } else {
+          selectedFile = LocalFile(
+            name: platformFile.name,
+            file: File(platformFile.path!),
+            size: platformFile.size,
+          );
+        }
+      return selectedFile;
+    }
+  } catch (e) {
+    if (context.mounted) {
+      showErrorSnackBar(context, "Error selecting files: ${e.toString()}");
+    }
+  }
+  return null;
+}
+
+Future<void> pickFiles(
+  int maxFiles,
+  BuildContext context,
+  List<AppFile> selectedFiles,
+  void Function(void Function()) setState,
+) async {
+  const maxFileSizeMB = 5;
+  const maxFileSizeBytes = maxFileSizeMB * 1024 * 1024;
+  try {
+    final result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+      type: FileType.custom,
+      withData: kIsWeb,
+      dialogTitle: _dialogTitle,
+      lockParentWindow: true,
+      allowCompression: false,
+      allowedExtensions: _allowedExtensions,
+    );
+
+    if (result != null && context.mounted) {
+      final newFileCount = result.files.length;
+      final totalFiles = selectedFiles.length + newFileCount;
+
+      if (totalFiles > maxFiles) {
+        showErrorSnackBar(context, "Maximum $maxFiles files allowed");
+        return;
+      }
+
+      final overSizedFiles = result.files
+          .where(
+            (file) => file.size > maxFileSizeBytes,
+          )
+          .toList();
+
+      if (overSizedFiles.isNotEmpty) {
+        showErrorSnackBar(
+          context,
+          "${overSizedFiles.length} file(s) exceed "
+          "${maxFileSizeBytes ~/ 1024 ~/ 1024}MB limits",
+        );
+        return;
+      }
+
+      setState(() {
+        selectedFiles.addAll(
+          result.files.map((PlatformFile platformFile) {
+            if (kIsWeb || platformFile.path == null) {
+              return WebFile(
+                name: platformFile.name,
+                bytes: platformFile.bytes!,
+                size: platformFile.size,
+              );
+            }
+            return LocalFile(
+              name: platformFile.name,
+              file: File(platformFile.path!),
+              size: platformFile.size,
+            );
+          }).toList(),
+        );
+      });
+    }
+  } catch (e) {
+    if (context.mounted) {
+      showErrorSnackBar(context, "Error selecting files: ${e.toString()}");
+    }
+  }
+}
+
+void removeFile(
+  List<AppFile> selectedFiles,
+  AppFile file,
+  void Function(void Function()) setState,
+) {
+  setState(() {
+    selectedFiles.remove(file);
+  });
 }
 
 String _getDownloadUrl(String originalUrl) {
